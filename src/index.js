6ebf687060a72184c2f6c4c252fc942cb14384a5
noe <noe.reyes92@yahoo.com>
2021-07-23 20:59:37 -0500

    commit tree b355ed85c9830feb2ece07b26d069e31fa0ba8a2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('%c HI', 'color: firebrick')
+const container = document.querySelector('#dog-image-container');
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+
+function getImages() {
+fetch(imgUrl)
+.then(response => response.json())
+.then(images => {
+    const imgs = images.message;
+    let imgsArray = createImgElement(imgs)
+    renderImgs(imgsArray)
+    })
+}
+
+function createImgElement(imgs) {
+    return imgs.map((img) => {
+        let i = `<img src=${img}>`;
+        console.log(i);
+        return i;
+    })
+}
+
+function renderImgs(imgsArray) {
+    imgsArray.forEach(element => {
+        renderElement(element)
+    })
+}
+
+function renderElement(element) {
+    container.innerHTML += element;
+}
+
+getImages()
